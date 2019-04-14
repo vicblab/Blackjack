716e037e15e8f72f3c3c00055a4f73636ac58d35
Víctor White <36444816+vicblab1@users.noreply.github.com>
2019-04-14 21:47:11 +0200

    CalculateProbabilities implementado

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -105,32 +105,203 @@ public class Deck : MonoBehaviour
     {
         for (int i = 0; i < 2; i++)
         {
-            PushPlayer();
             PushDealer();
+            PushPlayer();
+            
             /*TODO:
              * Si alguno de los dos obtiene Blackjack, termina el juego y mostramos mensaje
              */
             if (dealer.GetComponent<CardHand>().points == 21 && player.GetComponent<CardHand>().points != 21)
             {
-                finalMessage.text = "HAHAHAHAHAHA PRINGAO, TENIAS UN SOLO TRABAJO Y LA BANCA SACÓ BLACKJACK";
-            }else if (player.GetComponent<CardHand>().points == 21 && dealer.GetComponent<CardHand>().points != 21)
+                dealer.GetComponent<CardHand>().InitialToggle();
+                finalMessage.text = "TENIAS UN SOLO TRABAJO Y LA BANCA SACÓ BLACKJACK";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
+            }
+            else if (player.GetComponent<CardHand>().points == 21 && dealer.GetComponent<CardHand>().points != 21)
             {
-                finalMessage.text = "MU BIEN SUERTUDO DE MIERDA, BLACKJACK!";
-            }else if(player.GetComponent<CardHand>().points == 21 && dealer.GetComponent<CardHand>().points == 21)
+                dealer.GetComponent<CardHand>().InitialToggle();
+                finalMessage.text = "MU BIEN SUERTUDO, BLACKJACK!";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
+            }
+            else if(player.GetComponent<CardHand>().points == 21 && dealer.GetComponent<CardHand>().points == 21)
             {
+                dealer.GetComponent<CardHand>().InitialToggle();
                 finalMessage.text = "PERO QUE ME ESTAS CONTANDO, ESTO ES UN EMPATE";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
             }
         }
+        
     }
 
     private void CalculateProbabilities()
     {
         /*TODO:
          * Calcular las probabilidades de:
-         * - Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
-         * - Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
-         * - Probabilidad de que el jugador obtenga más de 21 si pide una carta          
+         * 1- Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
+         * 2- Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
+         * 3- Probabilidad de que el jugador obtenga más de 21 si pide una carta          
          */
+         //1------------------
+        int puntuacionVisible = dealer.GetComponent<CardHand>().points - dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().value;
+        int diferenciaPuntosInicial = player.GetComponent<CardHand>().points - puntuacionVisible;
+        int cartasBaraja = 52 - dealer.GetComponent<CardHand>().cards.Count - player.GetComponent<CardHand>().cards.Count;
+       float probDealerMasQueTu = 0;
+        int valorMaximoCarta = 21 - puntuacionVisible;
+        int numCartasGanadoras = 0;
+        if (valorMaximoCarta > 11)
+        {
+            valorMaximoCarta = 11;
+        }
+        int cont = 0;
+        
+            foreach(GameObject c in  dealer.GetComponent<CardHand>().cards)
+            {
+            if (c != dealer.GetComponent<CardHand>().cards[0])
+            {
+                if (c.GetComponent<CardModel>().value > diferenciaPuntosInicial && c.GetComponent<CardModel>().value < valorMaximoCarta)
+                {
+                    cont++;
+                }
+            }
+        }
+        foreach (GameObject c in player.GetComponent<CardHand>().cards)
+        {
+           
+                if (c.GetComponent<CardModel>().value > diferenciaPuntosInicial && c.GetComponent<CardModel>().value < valorMaximoCarta)
+                {
+                    cont++;
+                }
+            
+        }
+        Debug.Log(cont);
+        if (diferenciaPuntosInicial < 0)
+        {
+            diferenciaPuntosInicial = 0;
+        }
+        if (valorMaximoCarta < 10 && diferenciaPuntosInicial>0)
+        {
+            numCartasGanadoras = (valorMaximoCarta - diferenciaPuntosInicial) * 4 - cont;
+        } else if(valorMaximoCarta == 10)
+        {
+            numCartasGanadoras = (10 - diferenciaPuntosInicial) * 4 + 8 - cont;
+        }
+        else
+        {
+            numCartasGanadoras = (11 - diferenciaPuntosInicial) * 4 + 8 - cont;
+        }
+
+        probDealerMasQueTu = ((float)numCartasGanadoras / (float)cartasBaraja) * 100;
+        if (probDealerMasQueTu < 0)
+        {
+            probDealerMasQueTu = 0;
+        }
+
+        //2 ----------------------
+
+        int diecisiete = 17 - player.GetComponent<CardHand>().points;
+        int blackjack =  21 - player.GetComponent<CardHand>().points;
+        int cont2 = 0;
+        float probPuntuacionAlta = 0;
+        if (diecisiete < 0)
+        {
+            diecisiete = 0;
+        }
+        if (diecisiete > 11)
+        {
+            diecisiete = 11;
+        }
+        if (blackjack < 0)
+        {
+            blackjack = 0;
+        }
+        if (blackjack > 11)
+        {
+            blackjack = 11;
+        }
+        foreach (GameObject c in dealer.GetComponent<CardHand>().cards)
+        {
+           
+                if (c.GetComponent<CardModel>().value >= diecisiete && c.GetComponent<CardModel>().value <= blackjack)
+                {
+                    cont2++;
+                }
+            
+        }
+        bool isAce = false;
+        foreach (GameObject c in player.GetComponent<CardHand>().cards)
+        {
+
+            if (c.GetComponent<CardModel>().value >= diecisiete && c.GetComponent<CardModel>().value <= blackjack)
+            {
+                cont2++;
+            }
+            if (c.GetComponent<CardModel>().value == 11)
+            {
+                isAce = true;
+            }
+
+        }
+        if (blackjack < 10)
+        {
+            numCartasGanadoras = (blackjack - diecisiete) * 4 - cont2;
+        }else
+        {
+            numCartasGanadoras = (blackjack - diecisiete) * 4 +8- cont2;
+        }
+        if (numCartasGanadoras < 0)
+        {
+            numCartasGanadoras = 0;
+        }
+        probPuntuacionAlta= ((float)numCartasGanadoras / (float)cartasBaraja) * 100;
+        if (probPuntuacionAlta < 0)
+        {
+            probPuntuacionAlta = 0;
+        }
+
+        //3 --------------------------------------------------------------------------------------
+
+       float probMenosDiecisiete = 0;
+        if (isAce && player.GetComponent<CardHand>().points<12)
+        {
+            diecisiete += 10;
+        }
+            if (diecisiete < 10)
+            {
+                numCartasGanadoras = (diecisiete) * 4 - cont2;
+            }
+            else
+            {
+                numCartasGanadoras = (diecisiete) * 4 + 8 - cont2;
+            }
+        
+       
+        
+        if (numCartasGanadoras < 0)
+        {
+            numCartasGanadoras = 0;
+        }
+        probMenosDiecisiete = ((float)numCartasGanadoras / (float)cartasBaraja) * 100;
+        if (probMenosDiecisiete < 0)
+        {
+            probMenosDiecisiete = 0;
+        }
+        float probPasarse = 100 - probPuntuacionAlta - probMenosDiecisiete;
+        if (probPasarse < 0)
+        {
+            probPasarse = 0;
+        }
+        probMessage.text = "Prob.perder por carta oculta: "+probDealerMasQueTu.ToString()+"%\n " +
+            "Prob. obtener entre 17 y 21 en la siguiente: " + probPuntuacionAlta+ "%\n " +
+            "Prob. pasarse: "+ probPasarse+"%";
+        Debug.Log(diferenciaPuntosInicial);
+        Debug.Log(numCartasGanadoras);
+        Debug.Log(puntuacionVisible);
     }
 
     void PushDealer()
@@ -157,21 +328,32 @@ public class Deck : MonoBehaviour
         /*TODO: 
          * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
          */
-       //  if (player.GetComponent<CardHand>().)
-        
-        //Repartimos carta al jugador
-        PushPlayer();
+        //  if (player.GetComponent<CardHand>().)
 
-        /*TODO:
-         * Comprobamos si el jugador ya ha perdido y mostramos mensaje
-         */
-        if (player.GetComponent<CardHand>().points == 21)
+        //Repartimos carta al jugador
+        if (finalMessage.text == "")
         {
-            finalMessage.text = "MU BIEN SUERTUDO DE MIERDA, BLACKJACK!";
-        }
-        if ( player.GetComponent<CardHand>().points >21)
-        {
-            finalMessage.text = "HAS PERDIDO HIJO DE PUTA";
+            PushPlayer();
+
+            /*TODO:
+             * Comprobamos si el jugador ya ha perdido y mostramos mensaje
+             */
+            if (player.GetComponent<CardHand>().points == 21)
+            {
+                dealer.GetComponent<CardHand>().InitialToggle();
+                finalMessage.text = "MU BIEN SUERTUDO, BLACKJACK!";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+            "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+            "Prob. pasarse: -";
+            }
+            if (player.GetComponent<CardHand>().points > 21)
+            {
+                dealer.GetComponent<CardHand>().InitialToggle();
+                finalMessage.text = "HAS FRACASADO COMO EN TODO EN TU VIDA";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
+            }
         }
     }
 
@@ -186,7 +368,8 @@ public class Deck : MonoBehaviour
          * El dealer se planta al obtener 17 puntos o más
          * Mostramos el mensaje del que ha ganado
          */
-       
+        if (finalMessage.text == "")
+        {
             while (dealer.GetComponent<CardHand>().points < 17)
             {
                 PushDealer();
@@ -195,23 +378,42 @@ public class Deck : MonoBehaviour
             if (dealer.GetComponent<CardHand>().points == 21)
             {
                 finalMessage.text = "HAHAHA SACÓ BLACKJACK EL OTRO";
-
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
             }
             else if (dealer.GetComponent<CardHand>().points > 21)
             {
                 finalMessage.text = "AQUÍ PONE QUE HAS GANADO PERO NO SE YO E";
+                probMessage.text = "Prob. perder por carta oculta: - \n" +
+           "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+           "Prob. pasarse: -";
             }
             else if (dealer.GetComponent<CardHand>().points < 21)
             {
                 if (dealer.GetComponent<CardHand>().points > player.GetComponent<CardHand>().points)
                 {
                     finalMessage.text = "GANÓ LA BANCA WEY";
+                    probMessage.text = "Prob. perder por carta oculta: - \n" +
+            "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+            "Prob. pasarse: -";
                 }
-                else
+                else if(dealer.GetComponent<CardHand>().points < player.GetComponent<CardHand>().points)
                 {
                     finalMessage.text = "AQUÍ PONE QUE HAS GANADO PERO NO SE YO E";
+                    probMessage.text = "Prob. perder por carta oculta: - \n" +
+            "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+            "Prob. pasarse: -";
+                }
+                else if(dealer.GetComponent<CardHand>().points == player.GetComponent<CardHand>().points)
+                {
+                    finalMessage.text = "PERO QUE ME ESTAS CONTANDO, ESTO ES UN EMPATE";
+                    probMessage.text = "Prob. perder por carta oculta: - \n" +
+            "Prob. obtener entre 17 y 21 en la siguiente: - \n " +
+            "Prob. pasarse: -";
                 }
             }
+        }
         
     }
 
